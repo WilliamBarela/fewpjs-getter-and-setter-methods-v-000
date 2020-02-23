044f3e48b4434acfeea0b0729c364209f570d4a3
William Barela <william.barela.dev@gmail.com>
2020-02-23 23:48:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your Circle class here
+
+class Circle{
+  constructor(radius){
+    this.radius = radius;
+  }
+  
+  get diameter(){
+    this.diameter = 2 * this.radius;
+  }
+  
+  get circumference(){
+    this.circumference = ;
+  }
+}
